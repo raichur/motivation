29bb598ee798b15095c79ae1a64f60bfda6d85e1
Joshua Raichur <hello@joshuaraichur.com>
2015-07-26 22:13:59 +0530

    Update: Sleeping time. :sleeping:

--- vars.js
+++ vars.js
@@ -1,7 +1,7 @@
 // YOU NEED TO CHANGE THESE VARIABLES //
 
-var sleepingTimeHours = 9, // The hour you sleep. In my case I sleep at 9:30pm, so I entered 9
-sleepingTimeMinutes = 30, // The minutes remaining for the time you sleep. In my case I sleep at 9:30pm, so I entered 30
+var sleepingTimeHours = 12, // The hour you sleep. In my case I sleep at 9:30pm, so I entered 9
+sleepingTimeMinutes = 00, // The minutes remaining for the time you sleep. In my case I sleep at 9:30pm, so I entered 30
 yearOfBirth = 1997, // Year of birth
 monthOfBirth = 8, // Month of birth
 dayOfBirth = 31; // Day of birth
